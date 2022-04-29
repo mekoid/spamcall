5bc88db4fbd20d1f449481f8bf4862d913e36376
Eko Wahyudi <104310695+mekoid@users.noreply.github.com>
2022-04-29 20:43:39 +0700

    run sini
    apa itu console berwarna?:v

---
+++ spam.py
@@ -0,0 +1,9 @@
+import json
+from urllib import request
+
+print("Masukan nomor dengan format 8xxxx")
+no = input("masukan no: ")
+url = f"https://python-api-zhirrr.herokuapp.com/api/spamcall?no={no}"
+response = request.urlopen(url)
+data = json.loads(response.read())
+print (data['logs'])
